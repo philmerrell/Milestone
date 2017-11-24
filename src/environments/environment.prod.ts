d9a6a4d0948a1366de310e88fda824e1988167e3
Phil Merrell <philmerrell@boisestate.edu>
2017-11-23 23:51:58 -0700

    chore: Added envinroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCSe6E3y_uHDiCEtAeRtUw6meAV2TJpDVA',
+    authDomain: 'milestone-9ea70.firebaseapp.com',
+    databaseURL: 'https://milestone-9ea70.firebaseio.com',
+    projectId: 'milestone-9ea70',
+    storageBucket: '',
+    messagingSenderId: '69829832256'
+  }
 };
